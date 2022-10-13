31218f08839f2dc84d89d2889b302bdfea9345cc
Takadenoshi <tasos@kadena.io>
2022-10-13 10:49:15 +0200

    PageRef component fixes & improvements: - Fixed: url text did not align right when line wrapped - Improvement?: stripped uri scheme & www. subdomain from absolute urls - Improvement?: limit max url length - Improvement?: replace leading ../ recursively with /

--- src/components/PageRef.tsx
+++ src/components/PageRef.tsx
@@ -4,9 +4,48 @@ import styles from './PageRef.module.css';
 type PageRefProps = {
   url: string,
   pageName: string,
+  maxUrlLength?: number,
 }
 
-export default function PageRef({ url, pageName } : PageRefProps) {
+const schemeRegex = /https?:\/\//;
+
+/*
+ * Strip URI scheme (http) and www.
+ * e.g. `https://www.youtube.com/play` -> `//youtube.com/play`
+ */
+function stripUriScheme(url: string) {
+  if (schemeRegex.test(url)) {
+    const strippedScheme = url.slice(url.indexOf(':') + 1);
+    if (strippedScheme.startsWith('//www.')) {
+      return `//${strippedScheme.slice(6)}`;
+    }
+    return strippedScheme;
+  }
+  return url;
+}
+
+const upDirRegex = /^(\.\.\/)+/;
+
+/*
+ * Prettify URL for display
+ * - strip scheme, www. subdomain
+ * - strip ../../ prefix
+ * - enforce max length
+ */
+function prettyUrl(url: string, maxUrlLength: number): string {
+  let strippedUrl = stripUriScheme(url);
+  // remove leading ../../
+  while(upDirRegex.test(strippedUrl)) {
+    strippedUrl = strippedUrl.replace(upDirRegex, '/');
+  }
+  // enforce max length
+  if (strippedUrl.length > maxUrlLength) {
+    return `${strippedUrl.slice(0, maxUrlLength)}...`;
+  }
+  return strippedUrl;
+}
+
+export default function PageRef({ url, pageName, maxUrlLength=48 } : PageRefProps) {
   return (
     <a
       className={styles.pageRef}
@@ -16,7 +55,7 @@ export default function PageRef({ url, pageName } : PageRefProps) {
         <div className={styles.arrow}>&#8594;</div>
         <div className={styles.pageName}>{pageName}</div>
       </div>
-      <div className={styles.url}>{url}</div>
+      <div className={styles.url}>{prettyUrl(url, maxUrlLength)}</div>
     </a>
   )
 }
